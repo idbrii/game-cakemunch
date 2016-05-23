180d6d0e3347de1a9ae047277624774b7d46f0cf
David Briscoe <idbrii@gmail.com>
2016-05-23 14:02:06 -0700

    Setup basic scene with player controlled circle
    I've got a ball character and two walls.

---
+++ Assets/Scripts/MunchPlayerControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MunchPlayerControl : PlayerControl
+{
+
+    void Awake()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+}
+
