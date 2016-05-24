cef0798732a0aeb22a14305bb22e0ce45fb2fb8a
David Briscoe <idbrii@gmail.com>
2016-05-23 17:21:35 -0700

    Add a reflection that follows the player
    Update mob to support moving to target instead of only in a direction.
    Both UpdateMoveIntention and ForceMoveIntention_SkipPhysics work pretty
    well, but are quite different.

---
+++ Assets/Scripts/ReflectionMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectionMovement : Mob {
+
+    [Tooltip("The object we're reflecting.")]
+    public Transform original;
+
+    void Update() {
+        ForceMoveIntention_SkipPhysics(new Vector2(original.position.x, transform.position.y));
+
+        // This also works, but is much laggier.
+        //UpdateMoveIntention(new Vector2(original.position.x, transform.position.y));
+    }
+}
--- Assets/Scripts/brilib/game/Mob.cs
+++ Assets/Scripts/brilib/game/Mob.cs
@@ -9,11 +9,42 @@ public class Mob : MonoBehaviour
     private Vector2 movement;
 
 
+    protected void HaltMovement()
+    {
+        movement = new Vector2(0.0f, 0.0f);
+    }
+
     protected void UpdateMovement(float inputX, float inputY)
     {
         movement = new Vector2(
                 speed.x * inputX,
                 speed.y * inputY);
+    }
+
+    // A laggy move to target. This function is useful when you're trying to
+    // move somewhere specific and not just in a direction. It will avoid
+    // oscillating around your destination.
+    protected void UpdateMoveIntention(Vector2 desiredPosition)
+    {
+        float x = Mathf.MoveTowards(transform.position.x, desiredPosition.x, speed.x);
+        float y = Mathf.MoveTowards(transform.position.y, desiredPosition.y, speed.y);
+        x = x - transform.position.x;
+        y = y - transform.position.y;
+
+        movement = new Vector2(x, y);
+    }
+
+    // A snappier move to target that still respects 'speed'. Instead of using
+    // velocity, force the transform to our desired position. Useful when you
+    // don't want to lag caused by acceleration in physics. With really small
+    // values of speed (0.1), this function produces some soft but strict
+    // following behaviour.
+    protected void ForceMoveIntention_SkipPhysics(Vector2 desiredPosition)
+    {
+        float x = Mathf.MoveTowards(transform.position.x, desiredPosition.x, speed.x);
+        float y = Mathf.MoveTowards(transform.position.y, desiredPosition.y, speed.y);
+
+        transform.position = new Vector2(x, y);
     }
 
     void FixedUpdate()
